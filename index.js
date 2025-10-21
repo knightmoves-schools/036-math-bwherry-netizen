f85cc410cb2c903c11686ec3e736a1796d53e156
Brendon W <bwherry@midland.k12.ia.us>
2025-10-21 17:15:58 +0000

    gg

---
+++ index.js
@@ -0,0 +1,15 @@
+let total = 1 + 4;
+//total = total - 2;
+//total = total * 5;
+//total = total / 2;
+//let remainder = total % 2;
+
+
+
+
+
+
+
+
+
+document.getElementById("result").innerHTML = total;
