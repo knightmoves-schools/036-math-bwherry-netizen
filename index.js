501059ae165189bb0fda1a551a168a345a479adc
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-23 11:57:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let sum = 1 + 4;
-let difference = sum - 2;
-let product = sum * 5;
-let quotient = sum / 2;
+//let difference = sum - 2;
+//let product = sum * 5;
+//let quotient = sum / 2;
 let remainder = sum % 2;
 
 
@@ -12,4 +12,5 @@ let remainder = sum % 2;
 
 
 
-document.getElementById("result").innerHTML = quotient;
+//document.getElementById("result").innerHTML = quotient;
+document.getElementById("result").innerHTML = sum % 2;
