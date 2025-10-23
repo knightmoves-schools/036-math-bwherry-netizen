de554d6fe2a76efe7cc79196a6f836b5e15de8ee
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-23 11:58:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let sum = 1 + 4;
-//let difference = sum - 2;
-//let product = sum * 5;
-//let quotient = sum / 2;
+let difference = sum - 2;
+let product = sum * 5;
+let quotient = sum / 2;
 let remainder = sum % 2;
 
 
